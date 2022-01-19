5175a40f925847e0495731efd963ffca25522b0e
Jordan Devaney <jordandevaney28@gmail.com>
2022-01-18 19:43:23 -0500

    add structure for app

---
+++ public/resumeData.js
@@ -0,0 +1,175 @@
+export const resumeData = {
+	name: "Jordan Devaney",
+	occupation: "Full Stack Web Developer",
+	description:
+		"I have a strong desire to be a productive member of a team environment that fosters learning, growth, and develop a great product for the user",
+	image: "Me.jpg",
+	bio: "I am a Full Stack Web Developer passionate about solving complex problems and making great websites that will benefit the client. I am determined to serve you with my skills and meet your need.",
+	message: "Can I help you on your next project? Let's chat.",
+	email: "jordandevaney28@gmail.com",
+	phone: "810-220-9256",
+	address: {
+		street: "351 Emerald Ct E",
+		city: "Whitmore Lake",
+		state: "Michigan",
+		zip: 48189,
+	},
+	social: [
+		{
+			name: "linkedin",
+			url: "https://www.linkedin.com/in/jordandevaney/",
+			className: "fa fa-linkedin",
+		},
+		{
+			name: "github",
+			url: "https://github.com/devaneyj3",
+			className: "fa fa-github",
+		},
+	],
+	resume: {
+		skillmessage:
+			"I have developed superb in demand programming skills through BloomTech (AKA Lambda School), as well as, from various Udemy and other Massive Open Online Courses (MOOCs)",
+		education: [
+			{
+				school: "BloomTech (AKA Lamdba School)",
+				degree: "Full Stack Web Development and Computer Science",
+				graduated: "January 2021",
+				description:
+					"Worked on cross functional teams and building products with React, Redux, Context, Node, Python, and SQL",
+			},
+			{
+				school: "Spring Arbor University",
+				degree: "Bachelor of Arts: Children’s Ministry",
+				graduated: "May 2013",
+			},
+		],
+		work: [
+			{
+				company: "Floodgate Church",
+				title: "Freelance Web Developer",
+				years: "May 2021 - Current",
+				description: [
+					"Met with church's Graphic Designer to stragetize layout of website",
+					"Integrated church's events on an external CMS with Webflow",
+					"Translated mockups onto the Webflow UI",
+				],
+			},
+			{
+				company: "thesoftwareaccelerator.com",
+				title: "Founder/Content Creator",
+				years: "December 2020 - Current",
+				description: [
+					"Maintained WordPress website",
+					"Engaged in professional development video casts to polish and learn new skills",
+					"Incorporated design elements to enhance UX",
+					"Research article ideas and create content centered around web development",
+					"Utilized Copywriting strategies and affilate marketing to increase profit",
+					"Utilized SEO optimization to rank on Google and other search engines",
+				],
+			},
+			{
+				company: "DoorDash",
+				title: "Delivery Driver",
+				years: "January 2019 - December 2021",
+				description: [
+					"Comleted over 4000 deliveries with a 4.93 rating out of 5",
+					"Operated fast-paced delivery service throughout the Brighton market",
+					"Planned and strategized more efficient routes to increase speed of service",
+					"Received compliments for my timely service",
+				],
+			},
+		],
+		skills: [
+			{
+				name: "JavaScript",
+				level: "70%",
+			},
+			{
+				name: "Python",
+				level: "55%",
+			},
+			{
+				name: "ReactJS",
+				level: "80%",
+			},
+			{
+				name: "SCSS",
+				level: "60%",
+			},
+			{
+				name: "HTML5",
+				level: "80%",
+			},
+			{
+				name: "SQL",
+				level: "70%",
+			},
+			{
+				name: "Node",
+				level: "80%",
+			},
+			{
+				name: "AWS",
+				level: "50%",
+			},
+		],
+	},
+	portfolio: {
+		projects: [
+			{
+				title: "Anywhere Fitness",
+				category:
+					"A fitness management app where instructors can post classes and students can sign up.",
+				image: "fitness.jpg",
+				url: "https://inspiring-jones-a91418.netlify.app/",
+			},
+			{
+				title: "Simon Game",
+				category: "Replica of the old Simon Game.",
+				image: "simon-game.png",
+				url: "https://devaneyj3.github.io/simon_game/",
+			},
+			{
+				title: "Image Upload",
+				category:
+					"Used React and the React Uploading library to make my own image uploader.",
+				image: "image_upload.png",
+				url: "https://main.d39nx6sonuwpa1.amplifyapp.com/",
+			},
+			{
+				title: "Quote Maker",
+				category:
+					"Used React, React Router, SCSS, Reactstrap, and UUID to make a quote generator where users can enter in quotes.",
+				image: "quote_maker.png",
+				url: "https://main.d1w5rtjhri4um8.amplifyapp.com/quotes",
+			},
+			{
+				title: "To Do List",
+				category:
+					"Utilized Redux with React to make a todo app. Used AWS Amplify to deploy and the AWS Graphql API service.",
+				image: "todo.jpg",
+				url: "https://main.d1k25onjnr2iyp.amplifyapp.com/",
+			},
+			{
+				title: "Game of Life",
+				category:
+					"This project was a part of one of my build weeks at Lambda School. This was done using HTML Canvas, Vanilla JS and Bootstrap.",
+				image: "life.png",
+				url: "https://game-of-lif-jordan.netlify.app/",
+			},
+			{
+				title: "Budget App",
+				category: "Utilized React and Redux to make a buget and checkbook.",
+				image: "budget.png",
+				url: "https://dazzling-kirch-79dfd6.netlify.app/",
+			},
+			{
+				title: "Github Users",
+				category:
+					"Utilized React, Context API, GitHub API and Fusion Charts to display stats of searched user account.",
+				image: "github.png",
+				url: "https://search-github-users54.netlify.app/",
+			},
+		],
+	},
+};
