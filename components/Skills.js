c0f40b15beef85fd2ce33f1adc40acd43cb3050a
Jordan Devaney <jordandevaney28@gmail.com>
2022-02-13 16:57:18 -0500

    create contact form and send with nodemailer

--- components/Skills.js
+++ components/Skills.js
@@ -5,7 +5,7 @@ export default function Skills({ skills }) {
 	return (
 		<div>
 			<h1>SKILLS</h1>
-			<h1>I have used and am experieced in this technlogies: </h1>
+			<h1>I have used and am experieced in these technologies: </h1>{" "}
 			<ul>
 				{skills.map((skill) => {
 					return <li key={skill.label}>{skill.label}</li>;
